8426c0d488ffd989a08bc73a9c330c818093ceb1
kotyabuchi <kotyabuchi@gmail.com>
2023-02-15 15:44:41 +0900

    Add: VoiceChannelを生成する際に名前を変更できるように

--- src/main/kotlin/com/github/kotyabuchi/AllForOne/Command/RoomCommand.kt
+++ src/main/kotlin/com/github/kotyabuchi/AllForOne/Command/RoomCommand.kt
@@ -4,6 +4,8 @@ import net.dv8tion.jda.api.entities.channel.concrete.VoiceChannel
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceUpdateEvent
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent
+import net.dv8tion.jda.api.interactions.commands.OptionType
+import net.dv8tion.jda.api.interactions.commands.build.OptionData
 import net.dv8tion.jda.api.interactions.components.ActionRow
 import net.dv8tion.jda.api.interactions.components.buttons.Button
 
@@ -11,17 +13,19 @@ object RoomCommand: Command() {
     private val rooms = mutableMapOf<String, String>()
     override val name: String = "room"
     override val description: String = "一時的なボイスチャンネルを作成します。"
+    override val options: List<OptionData> = listOf(OptionData(OptionType.STRING, "name", "作成するチャンネル名", false))
     override val useEvent: Boolean = true
     override val action: SlashCommandInteractionEvent.() -> Unit = {
         val guild = guild
         val connectingChannelCategory = member?.voiceState?.channel?.parentCategory
+        val channelName = getOption("name")?.asString ?: "一時部屋"
         guild?.let {
-            guild.createVoiceChannel("一時部屋", connectingChannelCategory).queue {
-                reply("部屋を作成しました。ID: ${it.id}")
-                    .addActionRow(Button.danger("delete_room", "部屋を削除する。"))
+            guild.createVoiceChannel(channelName, connectingChannelCategory).queue {
+                reply("${channelName}を作成しました。ID: ${it.id}")
+                    .addActionRow(Button.danger("delete_room", "チャンネルを削除する。"))
                     .queue()
                 rooms[guild.id] = it.id
-                logger.info("Guild[${guild.id}]にVoiceChannel[一時部屋]を作成しました。")
+                logger.info("Guild[${guild.id}]にVoiceChannel[$channelName]を作成しました。")
             }
         }
     }
@@ -43,10 +47,10 @@ object RoomCommand: Command() {
             var button = event.component.asDisabled()
             button = if (channel == null) {
                 logger.info("Guild[${guild?.id}]のVoiceChannel[${channelId}]を見つけることが出来ませんでした。")
-                button.withLabel("部屋が存在しません。")
+                button.withLabel("チャンネルが存在しません。")
             } else {
                 deleteRoom(guild.id, channel)
-                button.withLabel("部屋を削除済です。")
+                button.withLabel("削除済です。")
             }
             event.editComponents(ActionRow.of(button)).queue()
         }
