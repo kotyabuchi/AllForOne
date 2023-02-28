712c754edf18037c3ef9beddfb23052fee6e8424
kotyabuchi <kotyabuchi@gmail.com>
2023-02-28 20:15:59 +0900

    Add: Jsoup,SQLite,exposedの依存関係を追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,14 @@
 val jdaVersion: String by project
 val logbackVersion: String by project
+val jsoupVersion: String by project
+val sqliteJDBCVersion: String by project
+val exposedVersion: String by project
 
 plugins {
     kotlin("jvm") version "1.8.0"
     application
+
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 group = "com.github.kotyabuchi"
@@ -17,13 +22,17 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("net.dv8tion:JDA:$jdaVersion")
     implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    implementation("org.jsoup:jsoup:$jsoupVersion")
+    implementation("org.xerial:sqlite-jdbc:$sqliteJDBCVersion")
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
 }
-
 
 kotlin {
     jvmToolchain(8)
 }
 
 application {
-    mainClass.set("com.github.kotyabuchi.AllForOne.Main.kt")
+    mainClass.set("com.github.kotyabuchi.AllForOne.Main")
 }
